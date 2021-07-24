2f1ee65e8246f838dc59bbdb05c712b3d74e25c6
Keyproco <oussama.keddar@epitech.eu>
2021-07-24 15:04:28 +0200

    Implementing Query Service

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 
 app.use(cors());
