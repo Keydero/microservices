c40b80a7ea7c6544e2fd6ae07725232e055e2e47
Keyproco <oussama.keddar@epitech.eu>
2021-07-24 14:43:21 +0200

    Recieving events

--- comments/index.js
+++ comments/index.js
@@ -14,15 +14,23 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostUid[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentUid = uuidv4();
     const { content } = req.body;
     const comments = commentsByPostUid[req.params.id] || [];
     comments.push({ id: commentUid, content });
     commentsByPostUid[req.params.id] = comments;
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: { commentUid, content, postId: req.params.id }
+    })
     res.status(201).send(comments);
 });
 
+app.post('/events', (req, res) => {
+    console.log('Receiving the event', req.body.type)
+    res.send({})
+});
 app.listen(4000, () => {
-    console.log("Listening port 4000")
+    console.log("COMMENTS: Listening port 4000")
 });
--- posts/index.js
+++ posts/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.json());
 const { v4: uuidv4 } = require('uuid');
-
+const axios = require('axios');
 const posts = {};
 const cors = require('cors');
 app.use(cors())
@@ -12,17 +12,29 @@ app.get('/posts', (req, res) => {
 });
 
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const uid = uuidv4();
     console.log(uid);
     const { title } = req.body;
     posts[uid] = {
         uid, title
     }
-    res.status(201).send(posts[uid])
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: { uid, title }
+    })
+    res.status(201).send(posts[uid]);
 });
 
+app.post('/events', (req, res) => {
+    console.log('Receiving the event', req.body.type)
+    res.send({})
+});
+
+app.post('/posts/events', (req, res) => {
+    console.log(res.body)
+})
 
 app.listen(4001, () => {
-    console.log('Listening on port 4001')
+    console.log('POSTS: Listening on port 4001')
 })
